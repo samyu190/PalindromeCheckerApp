4d81476d050f6e2abcc0242285d9bfcf85c41d03
samyu190 <SAMYUVELAVAN19@GMAIL.COM>
2026-03-25 09:42:30 +0530

    feature/UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+       System.out.println("Welcome to the palindrome Checker Mangement System");
+       System.out.println("Version : 1.0");
+       System.out.println("System initialized successfully.");
+    }
 }

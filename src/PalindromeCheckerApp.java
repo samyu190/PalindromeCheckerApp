96cfbdd502bbf86d66fe8ab3e6dca58853a5972a
samyu190 <SAMYUVELAVAN19@GMAIL.COM>
2026-03-25 10:05:23 +0530

    feature/UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,20 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-       System.out.println("Welcome to the palindrome Checker Mangement System");
-       System.out.println("Version : 1.0");
-       System.out.println("System initialized successfully.");
+        String str ="MOM";
+        String rev ="";
+
+        for (int i = str.length() -1;
+             i>= 0;i--){
+
+            rev = rev + str.charAt(i);
+        }
+        if(str.equals(rev)){
+            System.out.println("It is a palindrome");
+
+
+        }
+        else{
+            System.out.println("It is not a palindrome");
+        }
     }
 }
